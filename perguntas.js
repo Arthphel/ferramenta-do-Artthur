e51ed18b98f1d5dab09c6665da938f52b8de892d
Arthphel <artthur.costa@escola.pr.gov.br>
2025-07-30 08:12:23 -0300

    salvar

---
+++ perguntas.js
@@ -0,0 +1,16 @@
+criaCartao(
+    'Bleach'
+    'O que é zanpakutos?'
+    'são espadas especiais usadas pelos Shinigamis em Bleach para lutar contra Hollows e outros inimigos. Cada Zanpakuto é única, refletindo a alma e a personalidade de seu usuário, e possui duas formas de liberação: Shikai e bankai.'
+
+)
+criaCartao(
+    'Musica'
+    'Quem é slow gm?'
+    'Um artista com uma pegada mais melódica e pela mistura de trap com uma vibe introspectiva e sentimental. Ele tem bastante foco nas letras que abordam questões pessoais e cotidiana.'
+)
+criaCartao(
+    'Escrita'
+    'Quem é Rick Riordan ?'
+    'um autor americano, mais conhecido por escrever a série Percy Jackson e os Olimpianos. '
+)
