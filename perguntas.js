e98d226649f7cfe710a61cd8400c73f328d9ae17
Arthphel <artthur.costa@escola.pr.gov.br>
2025-07-30 08:39:26 -0300

    arrumando as perguntas

--- perguntas.js
+++ perguntas.js
@@ -1,16 +1,26 @@
 criaCartao(
-    'Bleach'
-    'O que é zanpakutos?'
-    'são espadas especiais usadas pelos Shinigamis em Bleach para lutar contra Hollows e outros inimigos. Cada Zanpakuto é única, refletindo a alma e a personalidade de seu usuário, e possui duas formas de liberação: Shikai e bankai.'
+    'Bleach',
+    'O que é zanpakutos?',
+    'são espadas especiais usadas pelos Shinigamis em Bleach para lutar contra Hollows e outros inimigos. Cada Zanpakuto é única, refletindo a alma e a personalidade de seu usuário, e possui duas formas de liberação: Shikai e bankai.',
 
 )
 criaCartao(
-    'Musica'
-    'Quem é slow gm?'
-    'Um artista com uma pegada mais melódica e pela mistura de trap com uma vibe introspectiva e sentimental. Ele tem bastante foco nas letras que abordam questões pessoais e cotidiana.'
+    'Musica',
+    'Quem é slow gm?',
+    'Um artista com uma pegada mais melódica e pela mistura de trap com uma vibe introspectiva e sentimental. Ele tem bastante foco nas letras que abordam questões pessoais e cotidiana.',
 )
 criaCartao(
-    'Escrita'
-    'Quem é Rick Riordan ?'
-    'um autor americano, mais conhecido por escrever a série Percy Jackson e os Olimpianos. '
+    'Escrita',
+    'Quem é Rick Riordan ?',
+    'um autor americano, mais conhecido por escrever a série Percy Jackson e os Olimpianos. ',
+)
+criaCartao(
+    'Filosofia',
+    'Qual é a filosofia de Friedrich Nietzsche',
+    'ela é chamada de nilismo onde se questiona ou nega o significado, valor e propósito da vida, da moralidade, da verdade e da existência em geral.',
+)
+criaCartao(
+    'Quadrinhos',
+    'Quem é o segundo robin ?',
+    'o segundo robin foi o jason todd que após uma votacao interativa da DC foi morto pelo coringa',
 )
